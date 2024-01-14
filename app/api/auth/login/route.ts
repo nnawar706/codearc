5b0187fdc7479c132e35999569241d6d4167023e
nnawar706 <nnawar706@gmail.com>
2024-01-14 18:29:18 +0600

    added cookie

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { sign } from "jsonwebtoken";
+import { serialize } from "cookie";
+
+export async function POST(request: Request) {
+    const body = await request.json();
+
+    const { secret_code } = body;
+
+    if (secret_code !== process.env.SECRET_CODE) {
+        return NextResponse.json(
+            {
+                status: false,
+                message: "Secret code does not match.",
+            },
+            {
+                status: 401,
+            }
+        );
+    }
+
+    const secret = process.env.JWT_SECRET || "";
+    const cookie = process.env.COOKIE_NAME || "";
+    const max_age = 60 * 60 * 24 * 30;
+
+    const token = sign(
+        {
+            id: process.env.USER_ID,
+        },
+        secret,
+        {
+            expiresIn: max_age,
+        }
+    );
+
+    const serialized = serialize(cookie, token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: max_age,
+        path: "/",
+    });
+
+    return NextResponse.json({
+        status: true,
+        message: "Welcome aboard, Nafisa!",
+    }, {
+        status: 200,
+        headers: { "Set-Cookie": serialized },
+    });
+}
